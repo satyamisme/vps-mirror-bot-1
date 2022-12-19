d89dad5f704718bd8fc5702346cf8c49bba1fbb1
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-12-19 20:14:50 +0530

    Update scrapper.py

--- bot/modules/scrapper.py
+++ bot/modules/scrapper.py
@@ -125,7 +125,26 @@ def scrapper(update, context):
          for a in links:
              link = a["href"]
              sendMessage(link,context.bot,update.message)
-             
+    elif "themoviesboss" in link:
+     client = requests.session()
+     r = client.get(url).text
+     soup = BeautifulSoup (r, "html.parser")
+     links = soup.select('a[href^="https://themoviesboss.mx/links/"]')
+     gd_txt = f"Total Links Found : {len(links)}\n\n"
+     for a in links:
+       link = a["href"]
+       r = client.get(link)
+       soup = BeautifulSoup(r.text, "html.parser")
+       links1 = soup.select('a[href^="https://links.inbbotlist.com/links/"]')
+       for a in links1:
+           link = a["href"]
+           r = client.get(link)
+           soup = BeautifulSoup(r.text, "html.parser")
+           links = soup.select('a[href^="https://filepress.lol/file/"]')
+           for a in links:
+                 link = a["href"]
+                 sendMessage(link,context.bot,update.message)  
+      
     elif "cinevood" in link:
         prsd = ""
         links = []
